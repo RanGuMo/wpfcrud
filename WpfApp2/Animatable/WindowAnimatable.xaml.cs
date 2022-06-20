ae00b51c157119d7b0e124eb76c7fb357a67cf7c
韦 <1126146235@qq.com>
2022-06-20 15:10:02 +0800

    添加了Container

--- WpfApp2/Animatable/WindowAnimatable.xaml.cs
+++ WpfApp2/Animatable/WindowAnimatable.xaml.cs
@@ -24,9 +24,19 @@ namespace WpfApp2.Animatable
             InitializeComponent();
         }
 
+        
+
         private void ThicknessAnimation_Completed(object sender, EventArgs e)
         {
-
+            System.Windows.Media.Animation.Storyboard sb = new System.Windows.Media.Animation.Storyboard();
+            System.Windows.Media.Animation.ThicknessAnimation dmargin = new System.Windows.Media.Animation.ThicknessAnimation();
+            dmargin.Duration = new TimeSpan(0, 0, 0, 0, 300);
+            dmargin.From = new Thickness(0, 300, 0, 300);
+            dmargin.To = new Thickness(0, 0, 0, 0);
+            System.Windows.Media.Animation.Storyboard.SetTarget(dmargin, GridMain);
+            System.Windows.Media.Animation.Storyboard.SetTargetProperty(dmargin, new PropertyPath("Margin", new object[] { }));
+            sb.Children.Add(dmargin);
+            sb.Begin();
         }
     }
 }
